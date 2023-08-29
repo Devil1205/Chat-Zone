06f9073a00573961cff63ee7cd37fdc7512d2c60
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2023-08-29 16:20:43 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,19 @@ import socketIO from 'socket.io-client';
 
 function App() {
   // console.log(socket);
-  const socket = socketIO.connect("http://localhost:5000");
+  // const socket = socketIO.connect("http://localhost:5000");
   // const socket = socketIO.connect("https://chat-zone-qu4q.onrender.com");
-  // const socket = socketIO.connect("https://chat-zone.vercel.app",);
+  const socket = socketIO("https://chat-zone.vercel.app",
+  {
+    withCredentials: false,
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          "my-custom-header" : "abcd"
+        }
+      }
+    }
+  });
 
   return (
     <Router>
--- server/index.js
+++ server/index.js
@@ -4,13 +4,20 @@ const server = require('http').createServer(app);
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const { Server } = require("socket.io");
+app.use(cors());
 const io = new Server(server,{
     cors: {
-      origin: "*"
+      origin: "*",
+      handlePreflightRequest: (req,res)=>{
+        res.writeHead(200,{
+            "Access-Control-Allow-origin":"*",
+            "Access-Control-Allow-Methods":"GET,POST",
+            "Access-Control-Allow-Headers": "my-custom-header"
+        })
+      }
     }
   });
 
-app.use(cors());
 const user = {};
 
 io.on('connection', socket =>{
